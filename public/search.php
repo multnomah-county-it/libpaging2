e374176ffc52aa8fca2fbbe1db19d083d4b3b38c
John Houser <john.houser@multco.us>
2022-09-23 07:23:56 -0700

    Added library to default search field list

--- public/search.php
+++ public/search.php
@@ -4,7 +4,7 @@
 $_GET['field_list'] = preg_replace('/[\x00-\x1F\x7F\xA0]/u', '', $_GET['field_list']);
 
 // Set the search values based on defaults and inputs
-$field_list = ! empty($_GET['field_list']) ? $_GET['field_list'] : 'key,author,title,bib{650_a,856_u},callList{callNumber,itemList{barcode}}';
+$field_list = ! empty($_GET['field_list']) ? $_GET['field_list'] : 'key,author,title,bib{650_a,856_u},callList{callNumber,itemList{barcode,library}}';
 $index = ! empty($_GET['index']) ? $_GET['index'] : 'SUBJECT';
 $terms = ! empty($_GET['terms']) ? $_GET['terms'] : '';
 $ct = ! empty($_GET['ct']) ? $_GET['ct'] : '50';
